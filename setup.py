a11a32c359688a76cb5ae3627f2f445bee4e194a
Chandrakanta Pal <pal.chandrakanta@gmail.com>
2018-12-27 20:23:12 +0530

    Releasing 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="kanpai",
-    version="0.0.5",
+    version="0.1.0",
     keywords="schema json validation request-validation input-validation",
     author="Chandrakanta Pal",
     author_email="pal.chandrakanta@gmail.com",
